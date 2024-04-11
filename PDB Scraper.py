80b716259e45bc69b31a3fa6de3413996097763d
Arif Maulana Azis <166703095+Arifmaulanaazis@users.noreply.github.com>
2024-04-11 19:46:48 +0700

    Update PDB Scraper.py

--- PDB Scraper.py
+++ PDB Scraper.py
@@ -1,15 +1,12 @@
-import requests, os, multiprocessing, time, sys
+import requests, os, multiprocessing
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.chrome.service import Service
 
 
